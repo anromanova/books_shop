aa3dd46ad0451d6e4a4faeef8f3395bc2f95aa71
Anastasia Romanova <anastasiya.erohina@gmail.com>
2022-05-13 00:20:56 +0100

    feat: add popup for cards

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+'use strict'
+
 // get data from json
 
-fetch('./books.json')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            let books = data.map((item, index) => ({...item, id: index}));
-            console.log(books);
-            showData(books);
-            return books;
-        });
+function mainPage() {
+    fetch('./books.json')
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        let books = data.map((item, index) => ({...item, id: index}));
+        console.log(books);
+        showData(books);
+        return books;
+    });
+}
 
 
 // show page content
-let body = document.querySelector('body');
+const body = document.querySelector('body');
 
 function showData(data) {
     let output = '';
@@ -24,17 +28,64 @@ function showData(data) {
         <p class="author">${item.author}</p>
         <p class="title">${item.title}</p>
         <p class="price">${item.price}$</p>
+        <div class="card_buttons">
+        <button class="item__button learn" type="button" book_id=${item.id}>Learn more</button>
+        <button class="item__button add_basket" type="button" book_id=${item.id}>Add to bag  <i class='bx bx-cart'></i></button></div>
         </div>`
+        // document.querySelectorAll('.item__button').forEach(button => {
+        //     button.setAttribute(book_id, data[index].id)
+        // });
     }
     document.querySelector('.book_cards').innerHTML = output;
+    const learnButton = document.querySelectorAll('.learn');
+    learnButton.forEach((button, index) => {
+        button.onclick = () => {
+            openModal(data, index);
+        }
+    })
+    // const cardButtons = document.querySelectorAll('.item__button');
+    // cardButtons.forEach(button => {
+    //         button.setAttribute('book_id', data[index]);
+    //     });
+    return learnButton;
 }
 
-
 function buildPage () {
+    mainPage();
     const title = document.createElement('h1');
-    title.classList.add('page_title')
+    const cart = document.createElement('div');
+    cart.classList.add('main_cart');
+    const cart_output =  `<i class='bx bx-cart'></i>`
+    cart.innerText = cart_output;
+    title.classList.add('page_title');
     title.innerText = 'Books shop';
-    body.appendChild(title);
+    body.append(title);
+    body.append(cart);
+
 }
 
 window.addEventListener('load', buildPage());
+
+
+// button "learn more and popup"
+
+// const learnButton = document.querySelectorAll('.learn');
+// console.log(learnButton)
+function openModal(data, index) { 
+    console.log(index)
+    let output = '';
+        output = `
+        <div class="popup_card">
+        <p class="author">${data[index].author}</p>
+        <p class="title">${data[index].title}</p>
+        <p class="description">${data[index].description}</p>
+        <button class="item__button close" type="button" book_id=${data[index].id}>Close</button>`
+        // - удаление всего контента со страницы?
+    body.innerHTML = output;
+    // document.querySelector('.popup_card').innerHTML = output;
+    const closePopupButton = document.querySelector('.close');
+        closePopupButton.onclick = () => {
+            body.innerHTML = '';
+            mainPage();
+        }
+}
